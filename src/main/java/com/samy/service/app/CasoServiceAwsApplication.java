8acce68a236aa459875f149de6f889425f1c2bef
jose <jose@gmail.com>
2021-07-05 18:06:31 -0500

    agregando cambios en el main

--- src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
+++ src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
@@ -1,17 +1,11 @@
 package com.samy.service.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.samy.service.app.repo.CasoRepo;
 
 @SpringBootApplication
 public class CasoServiceAwsApplication{
 
-	@Autowired
-	CasoRepo repo;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CasoServiceAwsApplication.class, args);
 	}
