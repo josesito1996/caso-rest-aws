7867e68f907757fb736403535196db8cb713ec0b
jose <jose@gmail.com>
2021-07-14 09:26:01 -0500

    cnanges

--- src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
+++ src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
@@ -1,9 +1,11 @@
 package com.samy.service.app;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,24 +23,38 @@ public class CasoServiceAwsApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        try {
-            String fileName = "proxy.conf";
-            String rutaLinux = "/etc/nginx/conf.d/";
-            File archivoConf = new File("src/main/resources/" + fileName);
-            File ficheroDestino = new File(rutaLinux, fileName);
-            log.info("Archivo pesa " + archivoConf.length() + " bytes");
-            if (archivoConf.exists()) {
-                if (!ficheroDestino.exists()) {
-                    Files.copy(Paths.get(archivoConf.getAbsolutePath()),
-                            Paths.get(ficheroDestino.getAbsolutePath()),
-                            StandardCopyOption.REPLACE_EXISTING);
-                    log.info("Archivo Copiado en Linux");
-                } else {
-                    log.info("Estas en Windows");
+        String fromFile = "src/main/resources/proxy.conf";
+        String toFile = "/etc/nginx/conf.d/01_proxy.conf";
+
+        boolean result = moveFile(fromFile, toFile);
+        log.info(result ? "Success! File moved (Éxito! Fichero movido)"
+                : "Error! Failed to move the file (Error! No se ha podido mover el fichero)");
+    }
+
+    private boolean moveFile(String fromFile, String toFile) {
+        File origin = new File(fromFile);
+        File destination = new File(toFile);
+        if (origin.exists()) {
+            try {
+                if (!destination.exists()) {
+                    destination.createNewFile();
                 }
-            }   
-        } catch (Exception e) {
-            log.error("Error : " + e.getMessage());
+                InputStream in = new FileInputStream(origin);
+                OutputStream out = new FileOutputStream(destination);
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+                return true;
+            } catch (IOException ioe) {
+                log.error("Error : " + ioe.getMessage());
+                return false;
+            }
+        } else {
+            return false;
         }
     }
 }
