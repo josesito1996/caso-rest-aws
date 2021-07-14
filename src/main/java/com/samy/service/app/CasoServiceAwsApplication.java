dc0daa1a0bebae7a6bb704aa7692e8433e0f5c4a
jose <jose@gmail.com>
2021-07-13 19:21:40 -0500

    implementando archivo conf al arrancar el proyecto de spring

--- src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
+++ src/main/java/com/samy/service/app/CasoServiceAwsApplication.java
@@ -1,12 +1,41 @@
 package com.samy.service.app;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CasoServiceAwsApplication{
+import lombok.extern.slf4j.Slf4j;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CasoServiceAwsApplication.class, args);
-	}
+@SpringBootApplication
+@Slf4j
+public class CasoServiceAwsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoServiceAwsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String fileName = "proxy.conf";
+        String rutaLinux = "/etc/nginx/conf.d/";
+        File archivoConf = new File("src/main/resources/" + fileName);
+        File ficheroDestino = new File(rutaLinux, fileName);
+        log.info("Archivo pesa " + archivoConf.length() + " bytes");
+        if (archivoConf.exists()) {
+            if (ficheroDestino.exists()) {
+                Files.copy(Paths.get(archivoConf.getAbsolutePath()),
+                        Paths.get(ficheroDestino.getAbsolutePath()),
+                        StandardCopyOption.REPLACE_EXISTING);
+                log.info("Archivo Copiado en Linux");
+            } else {
+                log.info("Estas en Windows");
+            }
+        }
+
+    }
 }
