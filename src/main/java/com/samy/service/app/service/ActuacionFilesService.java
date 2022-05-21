fc3bd14d92a2f3c280f228aa4f64c1235712d811
josesito1996 <85954217+josesito1996@users.noreply.github.com>
2022-05-21 17:13:38 -0500

    Revert "Feature branch samy"

--- src/main/java/com/samy/service/app/service/ActuacionFilesService.java
+++
@@ -1,9 +0,0 @@
-package com.samy.service.app.service;
-
-import com.samy.service.app.model.response.ActuacionFileResponse;
-
-public interface ActuacionFilesService {
-
-    public ActuacionFileResponse listarActuacionesConArchivos(String idCaso);
-    
-}
