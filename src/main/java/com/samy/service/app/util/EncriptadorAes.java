a2e81505a14e5011d218a9ef40af8b0ed17191d0
Jose Castillo <jcastillo@example.com>
2022-06-01 15:37:31 -0500

    modificando encriptador AES

--- src/main/java/com/samy/service/app/util/EncriptadorAes.java
+++ src/main/java/com/samy/service/app/util/EncriptadorAes.java
@@ -1,16 +1,16 @@
 package com.samy.service.app.util;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -30,72 +30,146 @@ public class EncriptadorAes {
 	@Value("${aws.config.encryption-key}")
 	private String encryptionKey;
 	
-	private SecretKeySpec crearClave(String clave) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-		byte[] claveEncriptacion = clave.getBytes("UTF-8");
+	@Value("${aws.config.encryption-iv}")
+	private String encryptionIv;
 
-		MessageDigest sha = MessageDigest.getInstance("SHA-1");
-
-		claveEncriptacion = sha.digest(claveEncriptacion);
-		claveEncriptacion = Arrays.copyOf(claveEncriptacion, 16);
-
-		SecretKeySpec secretKey = new SecretKeySpec(claveEncriptacion, "AES");
-
-		return secretKey;
+	/**
+	 * AES CBC encryption
+	 * 
+	 * @param message String to be encrypted
+	 * @param key     The key
+	 * @param iv      IV, Need and key Same length
+	 * @return Return encrypted ciphertext , Encoded as base64
+	 */
+	public String encryptCBC(String message) {
+		final String cipherMode = "AES/CBC/PKCS5Padding";
+		final String charsetName = "UTF-8";
+		try {
+			byte[] content = new byte[0];
+			content = message.getBytes(charsetName);
+//
+			byte[] keyByte = this.encryptionKey.getBytes(charsetName);
+			SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+//
+			byte[] ivByte = this.encryptionIv.getBytes(charsetName);
+			IvParameterSpec ivSpec = new IvParameterSpec(ivByte);
+			Cipher cipher = Cipher.getInstance(cipherMode);
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+			byte[] data = cipher.doFinal(content);
+			final Base64.Encoder encoder = Base64.getEncoder();
+			final String result = encoder.encodeToString(data);
+			return result;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
-	 * Aplica la encriptacion AES a la cadena de texto usando la clave indicada
+	 * AES CBC Decrypt
 	 * 
-	 * @param datos        Cadena a encriptar
-	 * @param claveSecreta Clave para encriptar
-	 * @return Información encriptada
-	 * @throws UnsupportedEncodingException
-	 * @throws NoSuchAlgorithmException
-	 * @throws InvalidKeyException
-	 * @throws NoSuchPaddingException
-	 * @throws IllegalBlockSizeException
-	 * @throws BadPaddingException
+	 * @param messageBase64 Ciphertext ,base64 code
+	 * @param key           The key , Same as when encrypting
+	 * @param iv            IV, Need and key Same length
+	 * @return Decrypted data
 	 */
-	public String encriptar(String datos)
-			throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException,
-			IllegalBlockSizeException, BadPaddingException {
-		SecretKeySpec secretKey = this.crearClave(this.encryptionKey);
-
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-		byte[] datosEncriptar = datos.getBytes("UTF-8");
-		byte[] bytesEncriptados = cipher.doFinal(datosEncriptar);
-		String encriptado = Base64.getEncoder().encodeToString(bytesEncriptados);
-
-		return encriptado;
+	public String decryptCBC(String messageBase64) {
+		final String cipherMode = "AES/CBC/PKCS5Padding";
+		final String charsetName = "UTF-8";
+		try {
+			final Base64.Decoder decoder = Base64.getDecoder();
+			byte[] messageByte = decoder.decode(messageBase64);
+//
+			byte[] keyByte = this.encryptionKey.getBytes(charsetName);
+			SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+//
+			byte[] ivByte = this.encryptionIv.getBytes(charsetName);
+			IvParameterSpec ivSpec = new IvParameterSpec(ivByte);
+			Cipher cipher = Cipher.getInstance(cipherMode);
+			cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+			byte[] content = cipher.doFinal(messageByte);
+			String result = new String(content, charsetName);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
-	 * Desencripta la cadena de texto indicada usando la clave de encriptacion
+	 * AES ECB encryption
 	 * 
-	 * @param datosEncriptados Datos encriptados
-	 * @param claveSecreta     Clave de encriptacion
-	 * @return Informacion desencriptada
-	 * @throws UnsupportedEncodingException
-	 * @throws NoSuchAlgorithmException
-	 * @throws InvalidKeyException
-	 * @throws NoSuchPaddingException
-	 * @throws IllegalBlockSizeException
-	 * @throws BadPaddingException
+	 * @param message String to be encrypted
+	 * @param key     The key
+	 * @return Return encrypted ciphertext , Encoded as base64
 	 */
-	public String desencriptar(String datosEncriptados)
-			throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException,
-			IllegalBlockSizeException, BadPaddingException {
-		SecretKeySpec secretKey = this.crearClave(this.encryptionKey);
+	public String encryptECB(String message, String key) {
+		final String cipherMode = "AES/ECB/PKCS5Padding";
+		final String charsetName = "UTF-8";
+		try {
+			byte[] content = new byte[0];
+			content = message.getBytes(charsetName);
+//
+			byte[] keyByte = key.getBytes(charsetName);
+			SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+			Cipher cipher = Cipher.getInstance(cipherMode);
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			byte[] data = cipher.doFinal(content);
+			final Base64.Encoder encoder = Base64.getEncoder();
+			final String result = encoder.encodeToString(data);
+			return result;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-		cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-		byte[] bytesEncriptados = Base64.getDecoder().decode(datosEncriptados);
-		byte[] datosDesencriptados = cipher.doFinal(bytesEncriptados);
-		String datos = new String(datosDesencriptados);
-
-		return datos;
+	/**
+	 * AES ECB Decrypt
+	 * 
+	 * @param messageBase64 Ciphertext ,base64 code
+	 * @param key           The key , Same as when encrypting
+	 * @return Decrypted data
+	 */
+	public String decryptECB(String messageBase64, String key) {
+		final String cipherMode = "AES/ECB/PKCS5Padding";
+		final String charsetName = "UTF-8";
+		try {
+			final Base64.Decoder decoder = Base64.getDecoder();
+			byte[] messageByte = decoder.decode(messageBase64);
+//
+			byte[] keyByte = key.getBytes(charsetName);
+			SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
+			Cipher cipher = Cipher.getInstance(cipherMode);
+			cipher.init(Cipher.DECRYPT_MODE, keySpec);
+			byte[] content = cipher.doFinal(messageByte);
+			String result = new String(content, charsetName);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
